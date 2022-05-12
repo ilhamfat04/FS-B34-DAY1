965985f5b233594294a7c1599951c864f495a37a
= <=>
2022-05-12 10:49:42 +0700

    update

--- src/component.js
+++ src/component.js
@@ -1,14 +1,37 @@
 import React from "react";
 
 // import component Header
+import Header from "./components/header";
 
 function Component() {
   return (
     //   Code Here
-    <div></div>
+    <div>
+      <Header />
+      <Content />
+      <Header />
+      <Button />
+    </div>
   );
 }
 
 // Create a new component here
+function Content() {
+  return (
+    <div>
+      <h1>
+        This is content, bro
+      </h1>
+    </div>
+  )
+}
+
+function Button() {
+  return (
+    <div>
+      <button>Click me</button>
+    </div>
+  )
+}
 
 export default Component;
--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,10 @@
 // Code Here
+function Header() {
+    return (
+        <div>
+            <h4>This is Header</h4>
+        </div>
+    )
+}
+
+export default Header
