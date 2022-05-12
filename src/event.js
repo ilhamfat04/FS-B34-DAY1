75e7dcee64777fd200b00204e755f9e2bd4eeb9e
= <=>
2022-05-12 11:28:50 +0700

    update

--- src/event.js
+++ src/event.js
@@ -2,10 +2,26 @@ import React from "react";
 
 function Event() {
   //Create function here
+  function greeting() {
+    return alert('Hello everyone there')
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        If you press
+        <b> Click Here</b> then the allert will be appear
+        {/* <button onClick={alert("Hello batch 34 FS")} > Click Here</button> */}
+        <button onClick={() => alert("Hello batch 34 FS")} > Click Here</button>
+      </p>
+      <p>
+        If you press
+        <b> Greeting</b> then the allert will be appear
+        {/* <button onClick={greeting()} > Greeting</button> */}
+        <button onClick={greeting} > Greeting</button>
+      </p>
+    </div>
   );
 }
 
