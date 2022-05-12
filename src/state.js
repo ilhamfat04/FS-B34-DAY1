8063853e9b08d17f902c96c27ae521c2905a2b6c
= <=>
2022-05-12 11:58:26 +0700

    update

--- src/state.js
+++ src/state.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function State() {
   //init State
+  // let name = "ilham fathullah"
+  // name = "jody septiawan"
+
+  const [counter, setCounter] = useState(0)
+  // counter = "jody septiawan"
 
   //Create Function Add
+  function add() {
+    setCounter(counter + 2)
+  }
 
   //Create Function Less
+  function less() {
+    setCounter(counter - 1)
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>If you click add button it will be increase by 1, and less button its opposite</p>
+      <h1>{counter}</h1>
+      <button onClick={add}>Add</button>
+      <button onClick={less}>Less</button>
+    </div>
   );
 }
 
