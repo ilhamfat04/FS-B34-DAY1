63c26fa19558dca0f2f7c6a435ae3a7b193e1b33
= <=>
2022-05-12 11:39:10 +0700

    update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,17 @@
 // code Here
+function List(props) {
+
+    console.log(props);
+
+    const { carName, color, qty } = props
+    return (
+        <div>
+            <h1>
+                {carName} - {color} - {qty}
+            </h1>
+            <button>{carName}</button>
+        </div>
+    )
+}
+
+export default List
--- src/props.js
+++ src/props.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 //Import Components
+import List from "./components/list";
 
 function Props() {
+
+  const name = "Buggati"
+  const color = "Blue"
+  const qty = "10"
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>Element img using default props</p>
+      <img src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Ym9va3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+
+
+      <List carName="Mercedez Benz" color="Red" qty="100" />
+      <List carName={name} color={color} qty={qty} />
+      <List />
+    </div>
+
   );
 }
 
